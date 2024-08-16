c59fc40dda7173897567bd238f8cbdd7d3c7f5df
Kypros Andreou <100076168+kyprosantreou@users.noreply.github.com>
2024-08-16 17:12:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,33 +11,9 @@ def load_dataframe():
 
     return df 
 
-def main():
-    df = load_dataframe()
-
-    # Selection menu
-    print("Select machine learning model:")
-    print("------------------------------")
-    print("(0) Exit")
-    print("(1) Decision Tree")
-    print("(2) Random Forest")
-    print("(3) K-Nearest Neighbour")
-    print("(4) Support Vector Machine (SVM)")
-    print("(5) Native Bayes")
-    print("(6) All the above")
-    print("\n")
-
+def get_user_choice():
     # List with the selections
     selections = [0, 1, 2, 3, 4, 5, 6]
-
-    # List with the algorithms
-    algorithms = {
-        1: ("Decision Tree", Decission_tree_train),
-        2: ("Random Forest", Random_forest_train),
-        3: ("K-Nearest Neighbour", KNN_train),
-        4: ("Support Vector Machine (SVM)", SVM_train),
-        5: ("Native Bayes", Native_bayes_train)
-    }
-
     # Checking if the user selection is invalid
     while True:
         try:
@@ -53,27 +29,82 @@ def main():
             print("Oops! That was not a valid input. Please enter a number.")
             print("\n")
 
-    # If the user's selection equals zero, the program stops.
+    return user_selection
+
+def single_algorithm_selection(algorithms,user_selection,df):
+    name, func = algorithms[user_selection]
+    print(f"{name}:")
+    print("-" * len(name))
+    func(df)
+
+
+def main():
+    df = load_dataframe()
+
+    # Selection menu
+    print("Select machine learning model:")
+    print("------------------------------")
+    print("(0) Exit")
+    print("(1) Decision Tree")
+    print("(2) Random Forest")
+    print("(3) K-Nearest Neighbour")
+    print("(4) Support Vector Machine (SVM)")
+    print("(5) Native Bayes")
+    print("(6) All the above")
+    print("\n")
+
+    # List with the algorithms
+    algorithms = {
+        1: ("Decision Tree", Decission_tree_train),
+        2: ("Random Forest", Random_forest_train),
+        3: ("K-Nearest Neighbour", KNN_train),
+        4: ("Support Vector Machine (SVM)", SVM_train),
+        5: ("Native Bayes", Native_bayes_train)
+    }
+
+    user_selection = get_user_choice()
+
     if user_selection == 0:
         print("Thank you for using my program!")
         exit(0)
 
-    # If the user's selection matches one of the predefined options in the list [1, 2, 3, 4, 5],
-    # retrieve the corresponding algorithm name and function from the 'algorithms' dictionary.
-    # Then, print the algorithm name followed by a line of dashes equal in length to the name.
-    # Finally, execute the selected function, passing in the dataframe 'df'. 
     elif user_selection in [1, 2, 3, 4, 5]:
-        name, func = algorithms[user_selection]
-        print(f"{name}:")
-        print("-" * len(name))
-        func(df)
+        single_algorithm_selection(algorithms, user_selection, df)
 
-    # If the user's selection equals to 6 the program is running all the algorithms
     elif user_selection == 6:
         for key, (name, func) in algorithms.items():
             print(f"{name}:")
             print("-" * (len(name)+1))
             func(df)
+            print("\n")
+
+    while True:
+        try:
+            run_again = input("Do you want to continue?\nType Y for yes or N for no: ")
+            print("\n")
+
+            if run_again == "Y":
+                user_selection = get_user_choice()
+
+                if user_selection == 0:
+                    print("Thank you for using my program!")
+                    exit(0)
+
+                elif user_selection in [1, 2, 3, 4, 5]:
+                    single_algorithm_selection(algorithms, user_selection, df)
+
+                elif user_selection == 6:
+                    for key, (name, func) in algorithms.items():
+                        print(f"{name}:")
+                        print("-" * (len(name)+1))
+                        func(df)
+                        print("\n")
+
+            elif run_again == "N":
+                print("Thank you for using my program!")
+                exit(0)
+        except ValueError:
+            print("Oops! That was not a valid input.")
             print("\n")
 
 if __name__ == "__main__":
